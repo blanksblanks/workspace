0a78ed9ac10ca34587840c139d31d982d980c190
Nina Baculinao <nb2406@columbia.edu>
2014-10-12 18:24:29 -0400

    Palindrome detector, overly complex expression tree, binary search tree with lazy deletion

--- HW2/src/Ex3_1.java
+++ HW2/src/Ex3_1.java
@@ -1,14 +1,38 @@
+/**
+ * 
+ * Exercise 3.1 clarification:
+ * 
+ * Please go to the Collection page on Java API (the book erroneously says
+ * "Collections". What you need to do is answer this question in Java using only
+ * the methods outlined in the Collection API.
+ * 
+ * http://docs.oracle.com/javase/7/docs/api/java/util/Collection.html
+ * 
+ * Do not use anything from sub-classes of Collection (e.g. no using get() in
+ * ArrayList). Only use the methods laid out in Collection.
+ * 
+ * As was stated in class, we want you to use proper Java here, but it doesn't
+ * have to compile (so you can omit some of the overhead if you want). At the
+ * same time, it would probably be beneficial to code this and just test it
+ * yourself.
+ * 
+ * Do NOT use the toArray() method. Blaer wants you to use the iterator()
+ * factory method in the Collection interface. You can use the iterator's
+ * methods because it is not a sub-class of Collection.
+ * 
+ */
+
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.ListIterator;
+import java.util.Iterator;
 
 public class Ex3_1 {
 
 	public static <T> void printLots(ArrayList<T> L,
 			ArrayList<Integer> P) {
 
-		ListIterator<T> elements = L.listIterator();
-		ListIterator<Integer> positions = P.listIterator();
+		Iterator<T> elements = L.iterator();
+		Iterator<Integer> positions = P.iterator();
 
 		int index = 0;
 		int position = positions.next();
@@ -34,9 +58,9 @@ public class Ex3_1 {
 
 	public static void main(String[] args) {
 
-		ArrayList<String> L = new ArrayList<String>(Arrays.asList("America",
-				"Bolivia", "China", "Denmark", "Egypt", "France", "Germany",
-				"Holland", "Iceland"));
+		ArrayList<String> L = new ArrayList<String>(Arrays.asList("Apples",
+				"Bananas", "Cantaloupes", "Dates", "Elderberries", "Figs", "Grapes",
+				"Honeydew melons", "Mangos"));
 		ArrayList<Integer> P = new ArrayList<Integer>(Arrays.asList(0, 2, 4, 6,
 				20));
 
