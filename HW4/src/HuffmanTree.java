8986ba90c7bcce056b046a8516141197df0eea08
Nina Baculinao <nb2406@columbia.edu>
2014-11-19 19:17:21 -0500

    Huge headway in print tree bfs method. now need to integrate with GUI

--- HW4/src/HuffmanTree.java
+++ HW4/src/HuffmanTree.java
@@ -13,7 +13,7 @@ public class HuffmanTree {
 	}
 	
 	private void buildHuffmanTree(BinaryHeap<HuffmanNode> heap) throws UnderflowException {
-		int num = 1; // initialize num at T1
+		int num = 1; // initialize at T1
 		while (heap.getCurrentSize() > 1) {
 			HuffmanNode right = heap.deleteMin();
 			HuffmanNode left = heap.deleteMin();
@@ -42,22 +42,59 @@ public class HuffmanTree {
     private void printTree(HuffmanNode t) {
         double nodes = 1.0;
         double level = 0.0;
+    	HuffmanNode blank = new HuffmanNode("blank");
+
         Queue<HuffmanNode> q = new LinkedList<HuffmanNode>();
         q.add(t);
+//        System.out.println("Added " + t.getCharacter() + " to queue");
         while (!q.isEmpty()) {
             HuffmanNode node = q.poll();
-            System.out.print(node + "\t"); // node
-            if (Math.pow(2.0, level) == nodes) {
-                level += 1.0;
-                System.out.println("\n");
-            }
-            else 
-                nodes += 1.0;
-            if (node.left != null) // draw \
+//            	System.out.println("Removed " + node.getCharacter() + " from queue");
+
+            if (node.left != null && node.right != null) {
+            	q.add(node.left);
+            	q.add(node.right);
+            	System.out.print("/\\");
+//            	System.out.println("Added " + node.left.getCharacter() + " and " + node.right.getCharacter() + " to queue");
+            } else if (node.left != null && node.right == null) { // draw /
                 q.add(node.left);
-            if (node.right != null)
-                q.add(node.right); // draw /
+                q.add(blank);
+            	System.out.print("/");
+//            	System.out.println("Added " + node.left.getCharacter() + " and blank to queue");
+            } else if (node.left == null && node.right != null) { // draw \
+            	q.add(blank);
+            	q.add(node.right);
+            	System.out.print("\\");
+//            	System.out.println("Added blank and " + node.right.getCharacter() + " to queue");
+            } else { // (node.left == null && node.right == null)
+//            	System.out.println("Reached leaf. do nothing");
+            }
+            
+            System.out.print(node + "\t"); // node
+//          System.out.println(Math.pow(2.0, level) + " compared to number of nodes " + nodes);
+          if (Math.pow(2.0, level) == nodes) {
+              level += 1.0;
+              System.out.println("\n");
+//              System.out.println("increased level by 1");
+          } else { 
+              nodes += (Math.pow(2.0, level) - nodes);
+//          	System.out.println("increased nodes by 1");
+          }
+          
+/*
+
+/\
+ t5
+/\/\
+t4 t3
+/\
+t2 e a s
+   /\
+sp t1
+  t nl
+
+ */
+        
         }
     }
-    
 }
