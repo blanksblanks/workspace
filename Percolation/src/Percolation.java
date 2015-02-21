f55b891c77aff88e378cb7e7d567a32533ef9fd9
Nina Baculinao <nb2406@columbia.edu>
2015-02-21 13:58:54 -0500

    feat(percolation): create nxn grid of closed sites and test

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,8 +1,60 @@
-
 public class Percolation {
-
+	
+	public int N;
+	public boolean[][] grid;
+	public WeightedQuickUnionUF uf;
+	
+	// create N-by-N grid, with all sites blocked
+	public Percolation(int N) {
+		this.N = N;
+		grid = new boolean[N][N];
+		for (int i = 1; i <= N; i++ ) {
+			for (int j = 0; j < N; j++) {
+				grid[i][j] = false;
+				System.out.println("Grid " + i + j + "is" + grid[i][j]);
+			}
+		}
+		uf = new WeightedQuickUnionUF(N * N + 2);
+	}
+	
+	// open site (row i, column j) if it is not open already
+	public void open(int i, int j) {
+		isIndexValid(i,j);
+		if (!isOpen(i, j))
+			grid[i][j] = true;		   
+	}
+	
+	// is site (row i, column j) open?
+	public boolean isOpen(int i, int j) {
+		int row = i - 1;
+		int col = j - 1;
+		return true;   
+	}
+	
+	// is site (row i, column j) full?
+	public boolean isFull(int i, int j) {
+		int row = i - 1;
+		int col = j - 1;
+		return true;   
+	}
+	   
+	// does the system percolate?
+	public boolean percolates() {
+		
+		   return true;
+	}
+	
+	public void isIndexValid(int i, int j) {
+        if (i < 0 || i > N-1 || j < 0 || j > N-1) {
+        	System.err.println("Out of bounds. Exiting.");
+        	System.exit(0);
+        }
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Percolation perc = new Percolation(10);
+		System.out.println(perc);
 	}
 
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,4 +1,28 @@
 
 public class PercolationStats {
+	
+	// perform T independent experiments on an N-by-N grid
+	public PercolationStats(int N, int T) {
+		
+	}
+	
+	// sample mean of percolation threshold
+	public double mean() {
+		return 1.0;
+	}
 
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return 1.0;
+	}
+	
+	// low  endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return 1.0;
+	}
+	
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return 1.0;
+	}
 }
